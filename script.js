02e1d5094c5a8482292df4321ad83de92ea146a3
Divya Gupta <117669677+Divsgupta12@users.noreply.github.com>
2023-05-03 12:14:51 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
 //your JS code here. If required.
+const timer= document.querySelector("#timer");
+function getTime(){
+	const now = new Date();
+	const hr = now.getHours();
+	const min = now.getMinutes();
+	const sec = now.getSeconds();
+	const year = now.getFullYear();
+	const month = now.getMonth()+1;
+	const day = now.getDate();
+	return {hr,min,sec,year,month,day};
+}
+function formateTime(time){
+    return `${time<10 ? "0" : ""}${time}`
+}
+function updateClock(){
+    const time = getTime();
+    const formateddate = `${time.year}/${formateTime(time.month)}/${formateTime(time.day)}`;
+    const formatedtime = `${formateTime(time.hr)}:${formateTime(time.min)}:${formateTime(time.sec)}`
+    timer.textContent = `${formateddate} ${formatedtime}`
+}
+setInterval(updateClock,1000);
